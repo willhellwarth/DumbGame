1d93ace25086b488f6c80b1de60a9363f55a5959
Will Watts <willadhell@gmail.com>
2016-12-09 16:59:09 -0800

    Update and rename Builder.cs to BuildScript.cs

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class Builder : ScriptableObject {
+public class BuildScript : ScriptableObject {
         static string[] SCENES = FindEnabledEditorScenes();
 
 	// Use real app name here
